7cd23ae4c93cd9e660af7928c84f9c4ca8fc1c46
Jordan <cj.rosasavila@gmail.com>
2017-05-05 11:34:41 -0500

    Get Catalogs and fill database on SplashScreen

--- app/src/main/java/com/pagatodo/yaganaste/ui/_controllers/SplashActivity.java
+++ app/src/main/java/com/pagatodo/yaganaste/ui/_controllers/SplashActivity.java
@@ -5,12 +5,21 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
+
 import com.pagatodo.yaganaste.App;
 import com.pagatodo.yaganaste.R;
+import com.pagatodo.yaganaste.data.DataSourceResult;
 import com.pagatodo.yaganaste.data.local.persistence.Preferencias;
-import com.pagatodo.yaganaste.net.RequestHeaders;
+import com.pagatodo.yaganaste.data.local.persistence.db.CatalogsDbApi;
+import com.pagatodo.yaganaste.data.model.webservice.request.adtvo.ObtenerCatalogoRequest;
+import com.pagatodo.yaganaste.data.model.webservice.response.adtvo.ObtenerCatalogosResponse;
+import com.pagatodo.yaganaste.exceptions.OfflineException;
+import com.pagatodo.yaganaste.net.ApiAdtvo;
+import com.pagatodo.yaganaste.net.IRequestResult;
 
-public class SplashActivity extends AppCompatActivity {
+import static com.pagatodo.yaganaste.utils.Recursos.CODE_OK;
+
+public class SplashActivity extends AppCompatActivity implements IRequestResult {
     private Preferencias pref;
 
     @Override
@@ -20,17 +29,50 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.splash_activity_layout);
 
         pref = App.getInstance().getPrefs();
-
+        final IRequestResult iRequestResult = this;
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                //Class clazz = !RequestHeaders.getTokenauth().isEmpty() ? SessionActivity.class : MainActivity.class;
-                Class clazz = MainActivity.class;
-                Intent intent = new Intent(SplashActivity.this,clazz);
-                startActivity(intent);
-                SplashActivity.this.finish();
+                try {
+                    ApiAdtvo.obtenerCatalogos(new ObtenerCatalogoRequest(), iRequestResult);
+                } catch (OfflineException e) {
+                    e.printStackTrace();
+                    callMainActivity();
+                }
             }
         }, 2000);
+    }
+
+    @Override
+    public void onSuccess(DataSourceResult result) {
+        switch (result.getWebService()) {
+            case OBTENER_CATALOGOS:
+                ObtenerCatalogosResponse response = (ObtenerCatalogosResponse) result.getData();
+                if (response.getCodigoRespuesta() == CODE_OK) {
+                    try {
+                        CatalogsDbApi api = new CatalogsDbApi(this);
+                        api.insertComercios(response.getData().getComercios());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                callMainActivity();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onFailed(DataSourceResult error) {
+        callMainActivity();
+    }
+
+    private void callMainActivity() {
+        Class clazz = MainActivity.class;
+        Intent intent = new Intent(SplashActivity.this, clazz);
+        startActivity(intent);
+        SplashActivity.this.finish();
     }
 }
