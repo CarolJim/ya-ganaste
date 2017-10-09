2e638f439b30044514965eca702fb63da98e0761
DESKTOP-86A0L9M\Francisco Manzo <dantestark2014@gmail.com>
2017-10-09 14:34:40 -0500

    Se agrego Fragmento restante

--- app/src/main/java/com/pagatodo/yaganaste/ui/maintabs/fragments/RecargasFormFragment.java
+++ app/src/main/java/com/pagatodo/yaganaste/ui/maintabs/fragments/RecargasFormFragment.java
@@ -155,7 +155,6 @@ public class RecargasFormFragment extends PaymentFormBaseFragment implements Pay
         if (favoriteItem != null) {
             recargaNumber.setText(favoriteItem.getReferencia());
             //recargaNumber.setEnabled(false);
-
             /**
              *
              */
