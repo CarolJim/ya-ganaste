c6c2a43d5f001ef535329db49eab020022ffd096
Ismael Cruz <icruz@Pagatodo.com>
2017-11-02 18:02:59 -0600

    Cambios en Fragment Informacion adicional 2

---
+++ app/src/main/java/com/pagatodo/yaganaste/ui/account/register/adapters/MontoMensualAdapter.java
@@ -0,0 +1,29 @@
+package com.pagatodo.yaganaste.ui.account.register.adapters;
+
+import android.content.Context;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+
+import com.pagatodo.yaganaste.interfaces.IOnSpinnerClick;
+import com.pagatodo.yaganaste.ui.account.register.interfaces.IAdaptadores;
+
+import java.util.List;
+
+/**
+ * Created by icruz on 02/11/2017.
+ */
+
+public class MontoMensualAdapter extends ArrayAdapter<String> implements IAdaptadores {
+
+    public MontoMensualAdapter(@NonNull Context context, @LayoutRes int resource,
+                               @NonNull List<String> objects, IOnSpinnerClick iOnSpinnerClick) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public String name() {
+        return "MontoMensual";
+    }
+}
---
+++ app/src/main/java/com/pagatodo/yaganaste/ui/account/register/interfaces/IAdaptadores.java
@@ -0,0 +1,9 @@
+package com.pagatodo.yaganaste.ui.account.register.interfaces;
+
+/**
+ * Created by icruz on 02/11/2017.
+ */
+
+public interface IAdaptadores {
+    String name();
+}
