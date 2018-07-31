2d2f61372354861abc1726bc4d671234f0316f2d
Omar Zuñiga <ozunigag@pagatodo.com>
2018-07-30 19:18:39 -0500

    * Incidencia consulta saldo operadores

--- app/src/main/java/com/pagatodo/yaganaste/ui_wallet/fragments/OperadorTabFragment.java
+++ app/src/main/java/com/pagatodo/yaganaste/ui_wallet/fragments/OperadorTabFragment.java
@@ -144,12 +144,12 @@ public class OperadorTabFragment extends SupportFragment implements OnClickItemH
                 Intent enabler = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivity(enabler);
             } else {
-                if (App.getInstance().getPrefs().loadDataInt(MODE_CONNECTION_DONGLE) == QPOSService.CommunicationMode.BLUETOOTH.ordinal()
+                /*if (App.getInstance().getPrefs().loadDataInt(MODE_CONNECTION_DONGLE) == QPOSService.CommunicationMode.BLUETOOTH.ordinal()
                         && App.getInstance().getPrefs().loadData(BT_PAIR_DEVICE).equals("")) {
                     element.setIdOperacion(OPTION_CONFIG_DONGLE);
                     UI.showErrorSnackBar(getActivity(), getString(R.string.please_config_dongle), Snackbar.LENGTH_SHORT);
                     goToWalletMainActivity();
-                } else {
+                } else {*/
                     UI.showAlertDialog(getContext(), getString(R.string.consultar_saldo_uyu_title), getString(R.string.consultar_saldo_uyu_desc),
                             getString(R.string.consultar_saldo_uyu_btn), (dialogInterface, i) -> {
                                 TransactionAdqData.getCurrentTransaction().setAmount("");
@@ -158,7 +158,7 @@ public class OperadorTabFragment extends SupportFragment implements OnClickItemH
                                 intentAdq.putExtra(TYPE_TRANSACTION, QPOSService.TransactionType.INQUIRY.ordinal());
                                 startActivity(intentAdq);
                             });
-                }
+                //}
             }
         } else {
             goToWalletMainActivity();
