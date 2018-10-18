cab76e44e60b1ef6cac1e5164a90f56ff561c253
Omar Zúñiga <ozunigag@pagatodo.com>
2018-10-18 08:41:30 -0500

    * Correccion incidencia orden alfabético de parentescos

--- app/src/main/java/com/pagatodo/yaganaste/interfaces/enums/Parentescos.java
+++ app/src/main/java/com/pagatodo/yaganaste/interfaces/enums/Parentescos.java
@@ -6,24 +6,24 @@ import com.pagatodo.yaganaste.interfaces.IEnumSpinner;
  * Created by Jordan on 03/08/2017.
  */
 
-public enum Parentescos implements IEnumSpinner{
+public enum Parentescos implements IEnumSpinner {
     PARENTESCO(0, "Parentesco"),
-    PADRE(1	, "Padre"),
-    MADRE(2	, "Madre"),
-    HERMANO(3	, "Hermano(a)"),
-    HIJO(4	, "Hijo(a)"),
-    HIJASTRO(5	, "Hijastro(a)"),
-    CONYUGE(6	, "Cónyuge"),
-    CONCUBINO(7	, "Concubino(a)"),
-    SUEGRO(8	, "Suegro(a)"),
-    CUNIADO(9	, "Cuñado(a)"),
-    NUERA(10, "Nuera"),
-    YERNO(11, "Yerno"),
     ABUELO(12, "Abuelo(a)"),
+    CONCUBINO(7, "Concubino(a)"),
+    CUNIADO(9, "Cuñado(a)"),
+    CONYUGE(6, "Cónyuge"),
+    HERMANO(3, "Hermano(a)"),
+    HIJASTRO(5, "Hijastro(a)"),
+    HIJO(4, "Hijo(a)"),
+    MADRE(2, "Madre"),
     NIETO(13, "Nieto(a)"),
-    TIO(14, "Tío(a)"),
+    NUERA(10, "Nuera"),
+    PADRE(1, "Padre"),
     PRIMO(15, "Primo(a)"),
-    SOBRINO(16, "Sobrino(a)");
+    SOBRINO(16, "Sobrino(a)"),
+    SUEGRO(8, "Suegro(a)"),
+    TIO(14, "Tío(a)"),
+    YERNO(11, "Yerno");
 
     private String name;
     private int id;
