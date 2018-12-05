6ca8befb5f542c6658c82bee8b5587d242579b98
Ismael Cruz <icruz@pagatodo.com>
2018-12-05 12:49:03 -0600

    Scann (merge)

--- app/src/main/java/com/pagatodo/yaganaste/modules/register/RegActivity.java
+++ app/src/main/java/com/pagatodo/yaganaste/modules/register/RegActivity.java
@@ -1,30 +1,24 @@
 package com.pagatodo.yaganaste.modules.register;
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.pagatodo.yaganaste.R;
-<<<<<<< HEAD
 import com.pagatodo.yaganaste.modules.components.StepBar;
-=======
+
 import com.pagatodo.yaganaste.interfaces.enums.Direction;
->>>>>>> dc94c8624ab7afced0b113bfaccb94abe5588267
+
 import com.pagatodo.yaganaste.ui._controllers.manager.LoaderActivity;
 
 public class RegActivity extends LoaderActivity implements RegContracts.Presenter{
 
-<<<<<<< HEAD
+
     private RegRouter router;
     private StepBar stepBar;
 
-=======
-    //private RegContracts.RegRouter router;
 
-    private RegContracts.Router router;
->>>>>>> dc94c8624ab7afced0b113bfaccb94abe5588267
 
     public static Intent createIntent(Activity activity){
         return new Intent(activity,RegActivity.class);
@@ -56,7 +50,6 @@ public class RegActivity extends LoaderActivity implements RegContracts.Presente
     public void showFragmentDomicilioIngresaCP(){
         router.showPrsonalAddress(Direction.FORDWARD);
     }
-<<<<<<< HEAD
 
     @Override
     public void nextStep() {
@@ -67,7 +60,7 @@ public class RegActivity extends LoaderActivity implements RegContracts.Presente
     public void backStep() {
         stepBar.backStep();
     }
-=======
+
     public void showFragmentDomicilioSelectCP(){
         router.showPrsonalAddressSelectCP(Direction.FORDWARD);
     }
@@ -75,5 +68,5 @@ public class RegActivity extends LoaderActivity implements RegContracts.Presente
         router.showBusinessData(Direction.FORDWARD);
     }
 
->>>>>>> dc94c8624ab7afced0b113bfaccb94abe5588267
+
 }
