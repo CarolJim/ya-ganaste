7cfd71beb7bba679d10c767ec23254190ebe7416
Ismael Cruz <icruz@pagatodo.com>
2018-12-18 11:53:44 -0600

    Edit QR

---
+++ app/src/main/java/com/pagatodo/yaganaste/modules/qr/operations/EditQr/EditQrFragment.java
@@ -0,0 +1,18 @@
+package com.pagatodo.yaganaste.modules.qr.operations.EditQr;
+
+import com.pagatodo.yaganaste.modules.qr.operations.QrOperationActivity;
+import com.pagatodo.yaganaste.ui._manager.GenericFragment;
+
+public class EditQrFragment extends GenericFragment {
+
+    private QrOperationActivity activity;
+
+    public static EditQrFragment newInstance(){
+        return new EditQrFragment();
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+}
