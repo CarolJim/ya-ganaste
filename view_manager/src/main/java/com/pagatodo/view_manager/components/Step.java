11310e7957e611482bd1c93ba1fdc3268785eb4b
Ismael Cruz <icruz@pagatodo.com>
2019-01-15 12:39:45 -0600

    StepBar incidencias

---
+++ view_manager/src/main/java/com/pagatodo/view_manager/components/Step.java
@@ -0,0 +1,26 @@
+package com.pagatodo.view_manager.components;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class Step extends LinearLayout {
+
+    public Step(Context context) {
+        super(context);
+    }
+
+    public Step(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Step(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void init(){
+
+    }
+}
