ddb8799eef6140d9febd77bdb0dc969fdaa10cbf
agalicia <agalicia@pagatodo.com>
2019-02-11 15:04:14 -0600

    show balance sendsfragment

--- app/src/main/java/com/pagatodo/yaganaste/ui/maintabs/fragments/SendsFragment.java
+++ app/src/main/java/com/pagatodo/yaganaste/ui/maintabs/fragments/SendsFragment.java
@@ -4,38 +4,34 @@ package com.pagatodo.yaganaste.ui.maintabs.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-import com.google.android.material.textfield.TextInputLayout;
+import com.pagatodo.view_manager.components.HeadWallet;
+import com.pagatodo.yaganaste.App;
 import com.pagatodo.yaganaste.R;
-import com.pagatodo.yaganaste.modules.emisor.PaymentToQR.operations.QrOperationActivity;
-import com.pagatodo.yaganaste.modules.newsend.SendNewActivity;
 import com.pagatodo.yaganaste.ui._manager.GenericFragment;
-import com.pagatodo.yaganaste.utils.customviews.StyleButton;
 
-import static com.pagatodo.yaganaste.modules.newsend.SendNewActivity.ID_ALL_FAVO;
+import static com.pagatodo.yaganaste.utils.Recursos.ADQUIRENTE_BALANCE;
+import static com.pagatodo.yaganaste.utils.Recursos.USER_BALANCE;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SendsFragment extends GenericFragment implements View.OnClickListener {
-
-    View rootView;
-
-    @BindView(R.id.btn_show_fav)
-    StyleButton btn_show_fav;
-
-
+    private HeadWallet headWallet;
+    private LinearLayout send_from_card, send_from_clabe, send_from_telephone, send_from_qr;
+    private View rootView;
 
     public SendsFragment() {
         // Required empty public constructor
     }
-    public static SendsFragment newInstance(){
+
+    public static SendsFragment newInstance() {
         return new SendsFragment();
     }
 
@@ -44,6 +40,12 @@ public class SendsFragment extends GenericFragment implements View.OnClickListen
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_sends, container, false);
+        send_from_card = (LinearLayout) rootView.findViewById(R.id.send_from_card);
+        send_from_telephone = (LinearLayout) rootView.findViewById(R.id.send_from_telephone);
+        send_from_clabe = (LinearLayout) rootView.findViewById(R.id.send_from_clabe);
+        send_from_qr = (LinearLayout) rootView.findViewById(R.id.send_from_qr);
+        headWallet= (HeadWallet) rootView.findViewById(R.id.headWallet);
+
         initViews();
         return rootView;
     }
@@ -51,21 +53,24 @@ public class SendsFragment extends GenericFragment implements View.OnClickListen
     @Override
     public void initViews() {
         ButterKnife.bind(this,rootView);
-        btn_show_fav.setOnClickListener(this::onClick);
-
+        send_from_card.setOnClickListener(this::onClick);
+        send_from_telephone.setOnClickListener(this::onClick);
+        send_from_clabe.setOnClickListener(this::onClick);
+        send_from_qr.setOnClickListener(this::onClick);
+        headWallet.setAmount(App.getInstance().getPrefs().loadData(USER_BALANCE));
     }
-
 
     @Override
     public void onClick(View v) {
-
-        switch (v.getId()){
-            case R.id.btn_show_fav:
-                startActivity(SendNewActivity.createIntent(getActivity(),ID_ALL_FAVO));
+        switch (v.getId()) {
+            case R.id.send_from_card:
                 break;
-
+            case R.id.send_from_telephone:
+                break;
+            case R.id.send_from_clabe:
+                break;
+            case R.id.send_from_qr:
+                break;
         }
-
-
     }
 }
