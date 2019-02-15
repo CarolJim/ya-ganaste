59dc719e6bde6ab6bd69316b0d6c1d4abbec25e6
Ismael Cruz <icruz@Pagatodo.com>
2019-02-15 10:40:28 -0600

    String resources

---
+++ view_manager/src/main/java/com/pagatodo/view_manager/recyclers/FavoritesRecycler.java
@@ -0,0 +1,21 @@
+package com.pagatodo.view_manager.recyclers;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class FavoritesRecycler extends LinearLayout {
+    public FavoritesRecycler(Context context) {
+        super(context);
+    }
+
+    public FavoritesRecycler(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FavoritesRecycler(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
